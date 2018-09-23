76e70e3d0f04fc9a3d30c0760db1922bdc0c537d
Pawel Plaszczyk <pawelp73@gmail.com>
2018-09-23 12:58:52 +0200

    code cleanup

--- app.js
+++ app.js
@@ -36,18 +36,13 @@ model = {
   pixelData: [],
   myRows: () => model.pixelArt.length,
   myColumns: () => model.pixelArt[0].length,
-  finalParams: {
-    x: 0,
-    y: 0
-  },
-
   initializeAndCalculate: function () {
     /*
     1. project the grid into viewport relative to its center
     2. calculate vectors of movement
     3. write starting and final positions for each pixel
     */
-    let quarter, counter = 0,
+    let counter = 0,
       address = '',
       coordX, coordY, sideA, sideB, vector, myObject;
     let width = view.updateViewport().myWidth,
@@ -64,30 +59,18 @@ model = {
           sideB = y * scale - (rows * scale) / 2;
           coordX = sideA + width / 2;
           coordY = sideB + height / 2;
-          console.log(`counter: ${counter}, A,B:  ${sideA}, ${sideB} x,y: ${x},${y}`);
           vector = this.determineVectors(y, x, sideA, sideB, width, height, counter);
           myObject = new model.Pixel(coordX, coordY, vector.xfinal, vector.yfinal, counter);
           this.pixelData.push(myObject);
           address = `<div id = "${counter}" class="proper"><data posX = ${coordX} posY =${coordY} ></data></div>`;
-          // targetX = ${vector.lx} targetY = ${vector.ly}
           document.querySelector('#wrapper').insertAdjacentHTML('beforeend', address);
-          //view.renderTarget(counter, coordX, coordY);
-         view.renderTarget(counter, vector.xfinal, vector.yfinal);
-          // view.renderAtBorder (counter, vector.xfinal, vector.yfinal);
-
+          view.renderTarget(counter, coordX, coordY);
+         //view.renderTarget(counter, vector.xfinal, vector.yfinal);
         }
       }
     }
-    let center = `<div id = "center" class ="proper" style = "left: ${width/2}px; top: ${height/2}px; color: blue;" ></div>`
-    document.querySelector('#wrapper').insertAdjacentHTML('beforeend', center);
   },
-  /*
- 1. determine pixel's quarter relative to the picture center
- 2. establish correct vector while exploding/imploding
- 3. move the vector up to the edge of closest viewport border
-  */
   determineVectors: (y, x, sideA, sideB, width, height, counter) => {
-
     let quarter, sideC, sinBeta, cosBeta, myfinalCoords, finalCoords, finalVector, finalCoordX, finalCoordY, xfinal, yfinal, deltaA, deltaB, deltaC;
     let quarterPlace = () => {
       if (sideA <= 0 && sideB <= 0) {
@@ -100,12 +83,8 @@ model = {
         return 3;
       };
     };
-
-
     let normalizeToAspect = () => {
-
       let delimiter = model.aspect >= 1 ? height : width;
-      //console.log(delimiter);
       let multiply = model.aspect >= 1 ? {
         x: model.aspect,
         y: 1
@@ -113,7 +92,6 @@ model = {
         x: 1,
         y: model.aspect
       };
-      //console.log(multiply);
       if (sideB >= sideA) {
         deltaB = delimiter / 2 - sideB;
         deltaC = Math.abs(deltaB) / cosBeta;
@@ -123,36 +101,24 @@ model = {
         deltaC = Math.abs(deltaA) / cosBeta;
         deltaB = sinBeta * deltaC;
       }
-      //console.log(deltaA, deltaB, deltaC);
       quarter === 1 || quarter === 3 ? xfinal = width / 2 - (sideA + deltaA) * multiply.x : xfinal = width / 2 + (sideA + deltaA) * multiply.x;
       quarter === 1 || quarter === 2 ? yfinal = height / 2 - (sideB + deltaB) * multiply.y : yfinal = height / 2 + (sideB + deltaB) * multiply.y;
-      //console.log(`yfinal:!!! ${yfinal} xfinal:!!! ${xfinal} sideA: ${sideA} sideB: ${sideB}`);
-
       return {
         xfinal,
         yfinal
       };
-
     };
-
     quarter = quarterPlace();
-    //console.log(`quarter: ${quarter}`)
     //determine quarter, we don't care about vector directions anymore therefore
     sideA = Math.abs(sideA);
     sideB = Math.abs(sideB);
     //and Pitagoras himself reveals the lenght of the very vector!
     sideC = Math.sqrt(sideA ** 2 + sideB ** 2);
-    //sideC = Math.sqrt(Math.pow(sideA, 2) + Math.pow(sideB, 2));
-    //console.log(sideA, sideB, sideC);
-    //determine angle functions
     sinBeta = sideB >= sideA ? sideA / sideC : sideB / sideC;
     cosBeta = sideB >= sideA ? sideB / sideC : sideA / sideC;
     return normalizeToAspect();
   }
 };
-
-
-
 
 view = {
   updateViewport: function () {
@@ -171,62 +137,25 @@ view = {
     myElem.style.top = `${Math.floor(yoff)}px`;
     myElem.style.backgroundColor = "#ff0000";
   },
-  renderAtBorder: function (elem, xtr, ytr) {
-    //console.log(elem, xtr, ytr);
-    let element = elem;
-    myElem = document.getElementById(element);
-    myElem.style.left = `${Math.floor(xtr)}px`;
-    myElem.style.top = `${Math.floor(ytr)}px`;
-    myElem.style.backgroundColor = "#ff0000";
-  }
 };
-
 
 controller = {
     init: model.initializeAndCalculate(),
+    myRandomizer: function(trans, ampli){
+      let speed = (Math.random()*ampli)+1;
+      return trans + ((trans*speed)/100); 
+    },
     play: function () {
-      console.log('OK')
-      let transitionSpeed = 2;
       let instance;
       model.pixelData.map(pixel => {
           instance = document.getElementById(pixel.id.toString());
-          console.log(pixel);
-          console.log(instance);
-          let tops = Math.floor(pixel.xfinal) + "px";
-          let lefs = Math.floor(pixel.yfinal) + "px";
-          //console.log(tops, lefs);
-          //TweenMax.to(instance, 3, {x:100});
-         /* 
-          TweenLite.set(pixel, {
-            opacity: "1",
-            width: Math.floor(pixel.x)+"px",
-            height: Math.floor(pixel.y)+"px"
-          });
-
-          TweenLite.from(pixel, transitionSpeed, {
+          TweenLite.from(instance, this.myRandomizer(2,50), {
             ease: Bounce.easeOut,
-            scale: 1,
+            scale: 7,
             opacity: 0,
-            top: Math.floor(pixel.xfinal) + "px",
-            left: Math.floor(pixel.yfinal) + "px"
-          })
-*/
-          /*
-              TweenLite.set(instance, {
-                opacity: "1",
-                width: "300px",
-                height: "100px"
-              });
-              let transitionSpeed = 2;
-              TweenLite.from(instance, transitionSpeed, {
-                ease: Bounce.easeOut,
-                scale: 12,
-                opacity: 0,
-                top: "300px",
-                left:"100px"
-              });
-             // tween =   
-          */
+            top: Math.floor(pixel.yfinal) + "px",
+            left: Math.floor(pixel.xfinal) + "px"
+          })     
       })
     }
 };
